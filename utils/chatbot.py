d6cd9a489fd4f5c96a8e0f2a08beb1bf32ffb0f8
Pratik Nagdeve <pratiknagdeve7@gmail.com>
2026-07-16 13:15:01 +0530

    Update Gemini model to gemini-2.5-flash-lite

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -9,7 +9,7 @@ load_dotenv()
 def get_llm():
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-lite",
+        model="gemini-2.5-flash-lite",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.2
     )
