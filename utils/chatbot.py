e72b9e9a0dcfee70291bcdd540e92f4b386a5d23
Pratik Nagdeve <pratiknagdeve7@gmail.com>
2026-07-16 15:38:15 +0530

    Fixed Gemini API configuration and updated Streamlit app

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -3,15 +3,16 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# Load environment variables from a .env file
 load_dotenv()
 
 
 def get_llm():
-
+    """Initializes and returns the Gemini LLM instance."""
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash-lite",
+        model="gemini-flash-latest",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
-        temperature=0.2
+        temperature=0.2,
     )
 
     return llm
